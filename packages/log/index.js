91d40cb208ba003b796e563c37c21de00a7277d1
Christoph Herrmann <christoph.herrmann@maxdome.de>
2017-05-26 16:32:18 +0200

    Opportunity to enable/disable the timestamp

--- packages/log/index.js
+++ packages/log/index.js
@@ -1,14 +1,15 @@
 const winston = require('winston');
 
-module.exports = ({ level } = {}) => {
+module.exports = ({ level, timestamp } = {}) => {
+  if (timestamp) {
+    timestamp = () => {
+      return new Date().toString();
+    };
+  }
   return new winston.Logger({
     level,
     transports: [
-      new winston.transports.Console({
-        timestamp: () => {
-          return new Date().toString();
-        },
-      }),
+      new winston.transports.Console({ timestamp }),
     ],
   });
 };
