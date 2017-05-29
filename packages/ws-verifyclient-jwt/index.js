afe6911723453e5330b2891e37b25de6b4493129
Christoph Herrmann <christoph.herrmann@maxdome.de>
2017-05-29 15:29:42 +0200

    Prettier

--- packages/ws-verifyclient-jwt/index.js
+++ packages/ws-verifyclient-jwt/index.js
@@ -8,7 +8,9 @@ module.exports = ({ jwtSecret }) => (info, callback) => {
     }
     const [scheme, token] = authorization.split(' ');
     if (scheme !== 'Bearer') {
-      throw new Error('Invalid scheme in authorization header, supported: Bearer');
+      throw new Error(
+        'Invalid scheme in authorization header, supported: Bearer'
+      );
     }
     if (!token) {
       throw new Error('Missing token in authorization header');
