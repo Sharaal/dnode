6984b51477e5bb04fc93424830afeb7473dac1a6
Christoph Herrmann <christoph.herrmann@maxdome.de>
2017-07-11 12:13:53 +0200

    Add support for ISO 8601 output format

--- test/packages/parse-duration/index.js
+++ test/packages/parse-duration/index.js
@@ -44,6 +44,16 @@ describe('/packages/parse-duration', () => {
     const expected = 4800;
     assert.deepEqual(actual, expected);
   });
+
+  it('1 hour 20 minutes in ISO 8601', () => {
+    const actual = parseDuration('1h 20m', 'ISO 8601');
+    const expected = 'PT1H20M';
+    assert.deepEqual(actual, expected);
+  });
+
+  it('1 day 4 hours 20 minutes 7 seconds and 87 milliseconds in ISO 8601', () => {
+    const actual = parseDuration('1d 4h 20m 7s 87ms', 'ISO 8601');
+    const expected = 'P1DT4H20M7.087S';
     assert.deepEqual(actual, expected);
   });
 });
