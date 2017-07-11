dbd8b51f749ef4d08d7bbfd4813e36dfaf864c18
Christoph Herrmann <christoph.herrmann@maxdome.de>
2017-07-11 11:47:20 +0200

    Fix actual/expected assigns

--- test/packages/parse-duration/index.js
+++ test/packages/parse-duration/index.js
@@ -4,44 +4,46 @@ const parseDuration = require('../../../packages/parse-duration');
 
 describe('/packages/parse-duration', () => {
   it('1 hour 20 minutes in milliseconds', () => {
-    const actual = 4800000;
-    const expected = parseDuration('1h 20m');
+    const actual = parseDuration('1h 20m');
+    const expected = 4800000;
     assert.deepEqual(actual, expected);
   });
 
   it('1 hour 20 minutes in seconds (with second parameter)', () => {
-    const actual = 4800;
-    const expected = parseDuration('1h 20m', 's');
+    const actual = parseDuration('1h 20m', 's');
+    const expected = 4800;
     assert.deepEqual(actual, expected);
   });
 
   it('1 hour 20 minutes in seconds (with second parameter and plural)', () => {
-    const actual = 4800;
-    const expected = parseDuration('1h 20m', 'seconds');
+    const actual = parseDuration('1h 20m', 'seconds');
+    const expected = 4800;
     assert.deepEqual(actual, expected);
   });
 
   it('1 hour 20 minutes in seconds (with "in" keyword)', () => {
-    const actual = 4800;
-    const expected = parseDuration('1h 20m in s');
+    const actual = parseDuration('1h 20m in s');
+    const expected = 4800;
     assert.deepEqual(actual, expected);
   });
 
   it('1 hour 20 minutes in seconds (with "in" keyword and plural)', () => {
-    const actual = 4800;
-    const expected = parseDuration('1h 20m in seconds');
+    const actual = parseDuration('1h 20m in seconds');
+    const expected = 4800;
     assert.deepEqual(actual, expected);
   });
 
   it('4800000 milliseconds in milliseconds', () => {
-    const actual = 4800000;
-    const expected = parseDuration(4800000);
+    const actual = parseDuration(4800000);
+    const expected = 4800000;
     assert.deepEqual(actual, expected);
   });
 
   it('4800000 milliseconds in seconds', () => {
-    const actual = 4800;
-    const expected = parseDuration(4800000, 'seconds');
+    const actual = parseDuration(4800000, 'seconds');
+    const expected = 4800;
+    assert.deepEqual(actual, expected);
+  });
     assert.deepEqual(actual, expected);
   });
 });
