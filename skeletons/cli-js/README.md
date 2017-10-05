2d5ed6503612646fdff7cfef440c251baf9e9d82
Christoph Herrmann <christoph.herrmann@maxdome.de>
2017-10-05 13:59:43 +0200

    Update skeletons

---
+++ skeletons/cli-js/README.md
@@ -0,0 +1,25 @@
+# Usage
+
+## local
+
+```bash
+npm i -S @dnode/name 
+```
+
+```json
+{
+  "scripts": {
+    "name": "dnode-name"
+  }
+}
+```
+
+## global
+
+```bash
+npm i -g @dnode/name 
+```
+
+```bash
+dnode-name
+```
--- skeletons/web/src/web.js
+++ skeletons/web/src/web.js
@@ -1,14 +1,7 @@
-require('dotenv-safe').config();
-
-const app = require('express')();
-app.disable('x-powered-by');
-
-require('@dnode/middlewares')(app, []);
-
-require('@dnode/controllers')(app, [require('./controller')()]);
-
-if (module.parent) {
-  module.exports = app;
-} else {
-  app.listen(process.env.PORT);
-}
+require('@dnode/env');
+require('@dnode/express')(app => {
+  require('@dnode/middlewares')(app, []);
+  require('@dnode/controllers')(app, [
+    require('./controller')(),
+  ]);
+});
