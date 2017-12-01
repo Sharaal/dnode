245a7f78719d1359c81261deb9ef5007d07c46d5
Christoph Herrmann <christoph.herrmann@maxdome.de>
2017-12-01 16:38:13 +0100

    Set NODE_ENV to production if its undefined

--- packages/env/index.js
+++ packages/env/index.js
@@ -6,3 +6,4 @@ if (fs.existsSync(path.join(process.cwd(), '.env.example'))) {
 } else if (fs.existsSync(path.join(process.cwd(), '.env'))) {
   require('dotenv').config();
 }
+process.env.NODE_ENV = process.env.NODE_ENV || 'production';
