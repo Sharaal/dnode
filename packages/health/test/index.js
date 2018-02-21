e295ecb6885719693b978b2ac79d569357b7db8f
Christoph Herrmann <christoph.herrmann@maxdome.de>
2018-02-21 16:54:01 +0100

    Simplify the tests

--- packages/health/test/index.js
+++ packages/health/test/index.js
@@ -5,118 +5,83 @@ const request = require('supertest');
 describe('packages/health', () => {
   it('should have outcome "UP" without any checks', done => {
     const app = express();
-
-    const health = require('../index')();
-    require('@dnode/controllers')(app, [health]);
-
+    require('@dnode/controllers')(app, [require('../index')()]);
     request(app)
       .get('/health')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(200)
       .then(res => {
-        assert.equal(res.body.outcome, 'UP');
-        assert.equal(res.body.checks.length, 0);
+        assert.deepEqual(res.body, { outcome: 'UP', checks: [] });
         done();
       });
   });
 
   it('should have outcome "UP" with positive checks', done => {
     const app = express();
-
-    const health = require('../index')({
+    require('@dnode/controllers')(app, [require('../index')({
       a: () => {},
       b: () => {},
-    });
-    require('@dnode/controllers')(app, [health]);
-
+    })]);
     request(app)
       .get('/health')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(200)
       .then(res => {
-        assert.equal(res.body.outcome, 'UP');
-        assert.equal(res.body.checks.length, 2);
-
-        assert.equal(res.body.checks[0].name, 'a');
-        assert.equal(res.body.checks[0].state, 'UP');
-        assert.deepEqual(res.body.checks[0].data, {});
-
-        assert.equal(res.body.checks[1].name, 'b');
-        assert.equal(res.body.checks[1].state, 'UP');
-        assert.deepEqual(res.body.checks[1].data, {});
-
+        assert.deepEqual(res.body, { outcome: 'UP', checks: [
+          { name: 'a', state: 'UP', data: {} },
+          { name: 'b', state: 'UP', data: {} },
+        ] });
         done();
       });
   });
 
   it('should have outcome "UP" with positive checks and data', done => {
     const app = express();
-
-    const health = require('../index')({
+    require('@dnode/controllers')(app, [require('../index')({
       a: { check: () => {}, data: { c: 'd' } },
       b: { check: () => {}, data: { e: 'f' } },
-    });
-    require('@dnode/controllers')(app, [health]);
-
+    })]);
     request(app)
       .get('/health')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(200)
       .then(res => {
-        assert.equal(res.body.outcome, 'UP');
-        assert.equal(res.body.checks.length, 2);
-
-        assert.equal(res.body.checks[0].name, 'a');
-        assert.equal(res.body.checks[0].state, 'UP');
-        assert.deepEqual(res.body.checks[0].data, { c: 'd' });
-
-        assert.equal(res.body.checks[1].name, 'b');
-        assert.equal(res.body.checks[1].state, 'UP');
-        assert.deepEqual(res.body.checks[1].data, { e: 'f' });
-
+        assert.deepEqual(res.body, { outcome: 'UP', checks: [
+          { name: 'a', state: 'UP', data: { c: 'd' } },
+          { name: 'b', state: 'UP', data: { e: 'f' } },
+        ] });
         done();
       });
   });
 
   it('should have outcome "DOWN" if at least one check throws an error', done => {
     const app = express();
-
-    const health = require('../index')({
+    require('@dnode/controllers')(app, [require('../index')({
       a: () => {
         throw new Error('test');
       },
       b: () => {},
-    });
-    require('@dnode/controllers')(app, [health]);
-
+    })]);
     request(app)
       .get('/health')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(500)
       .then(res => {
-        assert.equal(res.body.outcome, 'DOWN');
-        assert.equal(res.body.checks.length, 2);
-
-        assert.equal(res.body.checks[0].name, 'a');
-        assert.equal(res.body.checks[0].state, 'DOWN');
-        assert.deepEqual(res.body.checks[0].data, {});
-
-        assert.equal(res.body.checks[1].name, 'b');
-        assert.equal(res.body.checks[1].state, 'UP');
-        assert.deepEqual(res.body.checks[1].data, {});
-
+        assert.deepEqual(res.body, { outcome: 'DOWN', checks: [
+          { name: 'a', state: 'DOWN', data: {} },
+          { name: 'b', state: 'UP', data: {} },
+        ] });
         done();
       });
   });
 
   it('should support async checks', done => {
     const app = express();
-
-    const health = require('../index')({
+    require('@dnode/controllers')(app, [require('../index')({
       a: () => {
         return new Promise((resolve, reject) => {
           process.nextTick(() => {
@@ -125,26 +90,17 @@ describe('packages/health', () => {
         });
       },
       b: () => {},
-    });
-    require('@dnode/controllers')(app, [health]);
-
+    })]);
     request(app)
       .get('/health')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(500)
       .then(res => {
-        assert.equal(res.body.outcome, 'DOWN');
-        assert.equal(res.body.checks.length, 2);
-
-        assert.equal(res.body.checks[0].name, 'a');
-        assert.equal(res.body.checks[0].state, 'DOWN');
-        assert.deepEqual(res.body.checks[0].data, {});
-
-        assert.equal(res.body.checks[1].name, 'b');
-        assert.equal(res.body.checks[1].state, 'UP');
-        assert.deepEqual(res.body.checks[1].data, {});
-
+        assert.deepEqual(res.body, { outcome: 'DOWN', checks: [
+          { name: 'a', state: 'DOWN', data: {} },
+          { name: 'b', state: 'UP', data: {} },
+        ] });
         done();
       });
   });
