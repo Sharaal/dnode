bd789a891641bdfd934f5fa53d0cc68086eb0942
Christoph Herrmann <christoph.herrmann@maxdome.de>
2018-02-27 15:14:54 +0100

    Update "@dnode/log-middleware"

--- skeletons/web/src/web.js
+++ skeletons/web/src/web.js
@@ -5,14 +5,15 @@ const logger = require('@dnode/log')({
 });
 logger.info('initializing server');
 
-require('@dnode/express')(app => {
-  require('@dnode/middlewares')(app, [
-    require('@dnode/log-middleware')({ log: logger.info }),
-  ]);
-  require('@dnode/controllers')(app, [
-    require('@dnode/env').controller(),
-    require('@dnode/health').controller(),
-    require('@dnode/version').controller(),
-    require('./controller')()
-  ]);
-}, { log: logger.info });
+require('@dnode/express')(
+  app => {
+    require('@dnode/middlewares')(app, [require('@dnode/log-middleware').middleware({ log: logger.info })]);
+    require('@dnode/controllers')(app, [
+      require('@dnode/env').controller(),
+      require('@dnode/health').controller(),
+      require('@dnode/version').controller(),
+      require('./controller')(),
+    ]);
+  },
+  { log: logger.info }
+);
