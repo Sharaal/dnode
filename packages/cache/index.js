c5da721fb0b037d5f5d822b9fd7518b9072b8ccf
Christoph Herrmann <christoph.herrmann@maxdome.de>
2018-06-15 14:57:16 +0200

    Adopt to the new redis package

--- packages/cache/index.js
+++ packages/cache/index.js
@@ -1,27 +1,11 @@
-module.exports = client => async (key, get, options = {}) => {
+module.exports = redis => async (key, get, options = {}) => {
   if ((typeof options === 'object' && options.asSeconds) || typeof options === 'number') {
     options = { expire: options };
   }
-  if (typeof options.expire === 'object' && options.expire.asSeconds) {
-    options.expire = options.expire.asSeconds();
-  }
-  let value;
-  if (client.getJSON) {
-    value = await client.getJSON(key);
-  } else {
-    value = await client.get(key);
-  }
+  let value = await client.get(key);
   if (!value || (options.invalidate && options.invalidate(value))) {
     value = await get();
-    if (client.setJSON) {
-      await client.setJSON(key, value, options.expire);
-    } else {
-      if (options.expire) {
-        await client.set(key, value, 'EX', options.expire);
-      } else {
-        await client.set(key, value);
-      }
-    }
+    await client.set(key, value, options.expire);
   } else if (options.refresh) {
     await client.expire(key, options.expire);
   }
