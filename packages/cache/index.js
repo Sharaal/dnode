319e92c632f6c412bcf0deb4d3ac34562e13a0df
Christoph Herrmann <christoph.herrmann@maxdome.de>
2018-06-15 14:58:58 +0200

    Fix renaming variable

--- packages/cache/index.js
+++ packages/cache/index.js
@@ -2,12 +2,12 @@ module.exports = redis => async (key, get, options = {}) => {
   if ((typeof options === 'object' && options.asSeconds) || typeof options === 'number') {
     options = { expire: options };
   }
-  let value = await client.get(key);
+  let value = await redis.get(key);
   if (!value || (options.invalidate && options.invalidate(value))) {
     value = await get();
-    await client.set(key, value, options.expire);
+    await redis.set(key, value, options.expire);
   } else if (options.refresh) {
-    await client.expire(key, options.expire);
+    await redis.expire(key, options.expire);
   }
   return value;
 };
